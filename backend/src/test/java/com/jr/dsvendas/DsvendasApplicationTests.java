1ae1c378872c6259e57d436b9e69e579e938f6a2
v4lb3r <3cifhzht@duck.com>
2021-09-06 15:57:00 -0300

    project created

---
+++ backend/src/test/java/com/jr/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jr.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
